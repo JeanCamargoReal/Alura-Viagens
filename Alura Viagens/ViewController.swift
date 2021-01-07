23467612c63bcd4ae8fd7d5358e824d5c8803bf4
JeanCamargoReal <jeancamargoreal@gmail.com>
2021-01-07 09:44:17 -0300

    Listando o título das viagens

--- Alura Viagens/ViewController.swift
+++ Alura Viagens/ViewController.swift
@@ -7,11 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tabelaViagens: UITableView!
+    
+    @IBOutlet weak var viewHoteis: UIView!
+    
+    @IBOutlet weak var viewPacotes: UIView!
+    
+    
+    let listaViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return listaViagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let viagemAtual = listaViagens[indexPath.row]
+        
+        cell.textLabel?.text = viagemAtual.titulo
+        
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.tabelaViagens.dataSource = self
+        
+        self.viewPacotes.layer.cornerRadius = 10
+        self.viewHoteis.layer.cornerRadius = 10
     }
 
 
